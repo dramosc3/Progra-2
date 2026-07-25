e232f5c8f0cb19bfa1966c22697e424585c4349f
dramosc3 <dramosc3@miumg.edu.gt>
2026-07-24 23:22:01 -0600

    Format Ejercicio3.java for better readability

--- Lab I/src/Ejercicio3.java
+++ Lab I/src/Ejercicio3.java
@@ -1,1 +1,6 @@
-public class Ejercicio3{public static void main(String[]args){System.out.println("Ejercicio 3");}}
+public class Ejercicio3{
+  public static void main(String[] args){
+    
+    System.out.println("Ejercicio 3");
+  }
+}
