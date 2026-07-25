7700f2f9dc5bfffc227eaba40cde1526acfcbf7a
dramosc3 <dramosc3@miumg.edu.gt>
2026-07-24 23:21:28 -0600

    Format Ejercicio2.java for better readability

--- Lab I/src/Ejercicio2.java
+++ Lab I/src/Ejercicio2.java
@@ -1,1 +1,6 @@
-public class Ejercicio2{public static void main(String[]args){System.out.println("Ejercicio 2");}}
+public class Ejercicio2{
+    public static void main(String[] args){
+        
+        System.out.println("Ejercicio 2");
+    }
+}
