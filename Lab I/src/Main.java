0c9e383ec8d291e3d43f156fdce7187963b6d1c8
dramosc3 <dramosc3@miumg.edu.gt>
2026-07-24 23:27:10 -0600

    Format Main.java for better readability

--- Lab I/src/Main.java
+++ Lab I/src/Main.java
@@ -1,3 +1,9 @@
-public class Main{public static void main(String[]args){
- Persona p=new Docente("Luis",40,"Programación",8000); p.mostrarInformacion();
-}}
+public class Main{
+ public static void main(String[] args){
+ 
+  Persona p=new 
+  Docente("Luis",40,"Programación",8000); 
+  
+  p.mostrarInformacion();
+ }
+}
