0faf7cd6c1bb4b73a0a388c99c53fdd8c8d61e49
dramosc3 <dramosc3@miumg.edu.gt>
2026-07-24 23:28:13 -0600

    Refactor Persona class for better readability

--- Lab I/src/Persona.java
+++ Lab I/src/Persona.java
@@ -1,5 +1,12 @@
 public class Persona{
- protected String nombre; protected int edad;
- public Persona(String nombre,int edad){this.nombre=nombre;this.edad=edad;}
- public void mostrarInformacion(){System.out.println("Nombre: "+nombre+" Edad: "+edad);}
+ protected String nombre; 
+ protected int edad;
+ 
+ public Persona(String nombre,int edad){
+  this.nombre=nombre;
+  this.edad=edad;
+ }
+ public void mostrarInformacion(){
+  System.out.println("Nombre: "+nombre+" Edad: "+edad);
+ }
 }
