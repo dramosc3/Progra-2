9e21b48cd344a339f8ff28177ad739481eb6db07
dramosc3 <dramosc3@miumg.edu.gt>
2026-07-24 23:23:17 -0600

    Format Ejercicio4 class for better readability

--- Lab I/src/Ejercicio4.java
+++ Lab I/src/Ejercicio4.java
@@ -1,1 +1,6 @@
-public class Ejercicio4{public static void main(String[]args){System.out.println("Ejercicio 4");}}
+public class Ejercicio4{
+  public static void main(String[] args){
+    
+    System.out.println("Ejercicio 4");
+  }
+}
